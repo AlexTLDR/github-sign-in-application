1524e8f3282c3dec62940dd2bd0849e7c48f94ed
AlexFZR <abadragan@gmail.com>
2023-03-30 15:55:47 +0200

    setting the cookie

--- milestone-1/index_handler.go
+++ milestone-1/index_handler.go
@@ -8,4 +8,16 @@ import (
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintf(w, "Hello from \\!\n")
+	cookie := http.Cookie{
+		Name:     "exampleCookie",
+		Value:    "Hello world!",
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, &cookie)
+	w.Write([]byte("cookie set!"))
+
 }
